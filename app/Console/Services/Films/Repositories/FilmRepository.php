2417108d4a702cefe8115f73bc55c1472c706b45
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-12 00:39:29 +0300

    scraping from nodes

--- app/Console/Services/Films/Repositories/FilmRepository.php
+++ app/Console/Services/Films/Repositories/FilmRepository.php
@@ -2,12 +2,27 @@
 
 namespace App\Console\Services\Films\Repositories;
 
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 use App\Console\Services\Films\Contracts\FilmContract;
 
 class FilmRepository implements FilmContract
 {
+    protected $client;
+
+    protected $url = 'https://www.lostfilm.tv/new';
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function fetch()
     {
-        dd('working');
+        $response = $this->client->request('GET', $this->url);
+
+        $films = $response->filter('div.row > a')->each(function ($node) {
+            dump($node->filter('div.name-ru')->extract('_text'));
+        });
     }
 }
